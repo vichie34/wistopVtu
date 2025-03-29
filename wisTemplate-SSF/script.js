5eb343b90104198a062e12bbcde652396b8279fc
Victor Chukwuma <victorchuma1b3@gmail.com>
2025-03-29 03:18:45 +0100

    Connectiong FE with BE

---
+++ wisTemplate-SSF/script.js
@@ -0,0 +1,27 @@
+const fetch = window.fetch;
+
+// BE apiEndpoint URL
+const apiEndpoint = "";
+
+async function getData() {
+    try {
+        // make GET request
+        const response = await fetch(`${apiEndpoint}/data`);
+        const data = await response.json();
+        document.getElementById("data-container").innerHTML = data.message;
+    }
+
+    // update the web page with received data
+    catch (error) {
+        console.error("Error fetching data", error);
+    }    
+}
+
+const response = await fetch(`${apiEndpoint}/data`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json", 
+    },
+    body: JSON.stringify(userData),
+});
+const data = await response.json();
